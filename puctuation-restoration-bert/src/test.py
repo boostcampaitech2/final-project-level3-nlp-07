a8f5f8767f0105df4da975b39e51d1acfe7afef4
Doohae <rick7213@gmail.com>
2021-12-22 19:11:27 +0000

    <Punct Model> initial code

---
+++ puctuation-restoration-bert/src/test.py
@@ -0,0 +1,123 @@
+import os
+import torch
+from tqdm import tqdm
+import numpy as np
+
+import argparse
+from dataset import Dataset
+from model import DeepPunctuation, DeepPunctuationCRF
+from config import *
+
+
+parser = argparse.ArgumentParser(description='Punctuation restoration test')
+parser.add_argument('--cuda', default=True, type=lambda x: (str(x).lower() == 'true'), help='use cuda if available')
+parser.add_argument('--pretrained_model', default='klue/roberta-small', type=str, help='pretrained language model')
+parser.add_argument('--lstm_dim', default=-1, type=int, help='hidden dimension in LSTM layer, if -1 is set equal to hidden dimension in language model')
+parser.add_argument('--use_crf', default=False, type=lambda x: (str(x).lower() == 'true'), help='whether to use CRF layer or not')
+parser.add_argument('--data_path', default='../data/', type=str, help='path to test datasets')
+parser.add_argument('--weight_path', default='out/weights.pt', type=str, help='model weight path')
+parser.add_argument('--sequence_length', default=256, type=int, help='sequence length to use when preparing dataset (default 256)')
+parser.add_argument('--batch_size', default=8, type=int, help='batch size (default: 8)')
+parser.add_argument('--save_path', default='out/', type=str, help='model and log save directory')
+
+args = parser.parse_args()
+
+
+# tokenizer
+tokenizer = MODELS[args.pretrained_model][1].from_pretrained(args.pretrained_model)
+token_style = MODELS[args.pretrained_model][3]
+
+test_files = os.listdir(args.data_path)
+test_set = []
+for file in test_files:
+    test_set.append(Dataset(os.path.join(args.data_path, file), tokenizer=tokenizer, sequence_len=args.sequence_length,
+                            token_style=token_style, is_train=False))
+
+# Data Loaders
+data_loader_params = {
+    'batch_size': args.batch_size,
+    'shuffle': False,
+    'num_workers': 0
+}
+
+test_loaders = [torch.utils.data.DataLoader(x, **data_loader_params) for x in test_set]
+
+# logs
+model_save_path = args.weight_path
+log_path = os.path.join(args.save_path, 'logs_test.txt')
+
+# Model
+device = torch.device('cuda' if (args.cuda and torch.cuda.is_available()) else 'cpu')
+if args.use_crf:
+    deep_punctuation = DeepPunctuationCRF(args.pretrained_model, freeze_bert=False, lstm_dim=args.lstm_dim)
+else:
+    deep_punctuation = DeepPunctuation(args.pretrained_model, freeze_bert=False, lstm_dim=args.lstm_dim)
+deep_punctuation.to(device)
+
+
+def test(data_loader):
+    """
+    :return: precision[numpy array], recall[numpy array], f1 score [numpy array], accuracy, confusion matrix
+    """
+    num_iteration = 0
+    deep_punctuation.eval()
+    # +1 for overall result
+    tp = np.zeros(1+len(punctuation_dict), dtype=np.int)
+    fp = np.zeros(1+len(punctuation_dict), dtype=np.int)
+    fn = np.zeros(1+len(punctuation_dict), dtype=np.int)
+    cm = np.zeros((len(punctuation_dict), len(punctuation_dict)), dtype=np.int)
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for x, y, att, y_mask in tqdm(data_loader, desc='test'):
+            x, y, att, y_mask = x.to(device), y.to(device), att.to(device), y_mask.to(device)
+            y_mask = y_mask.view(-1)
+            if args.use_crf:
+                y_predict = deep_punctuation(x, att, y)
+                y_predict = y_predict.view(-1)
+                y = y.view(-1)
+            else:
+                y_predict = deep_punctuation(x, att)
+                y = y.view(-1)
+                y_predict = y_predict.view(-1, y_predict.shape[2])
+                y_predict = torch.argmax(y_predict, dim=1).view(-1)
+            num_iteration += 1
+            y_mask = y_mask.view(-1)
+            correct += torch.sum(y_mask * (y_predict == y).long()).item()
+            total += torch.sum(y_mask).item()
+            for i in range(y.shape[0]):
+                if y_mask[i] == 0:
+                    # we can ignore this because we know there won't be any punctuation in this position
+                    # since we created this position due to padding or sub-word tokenization
+                    continue
+                cor = y[i]
+                prd = y_predict[i]
+                if cor == prd:
+                    tp[cor] += 1
+                else:
+                    fn[cor] += 1
+                    fp[prd] += 1
+                cm[cor][prd] += 1
+    # ignore first index which is for no punctuation
+    tp[-1] = np.sum(tp[1:])
+    fp[-1] = np.sum(fp[1:])
+    fn[-1] = np.sum(fn[1:])
+    precision = tp/(tp+fp)
+    recall = tp/(tp+fn)
+    f1 = 2 * precision * recall / (precision + recall)
+
+    return precision, recall, f1, correct/total, cm
+
+
+def run():
+    deep_punctuation.load_state_dict(torch.load(model_save_path))
+    for i in range(len(test_loaders)):
+        precision, recall, f1, accuracy, cm = test(test_loaders[i])
+        log = test_files[i] + '\n' + 'Precision: ' + str(precision) + '\n' + 'Recall: ' + str(recall) + '\n' + \
+            'F1 score: ' + str(f1) + '\n' + 'Accuracy:' + str(accuracy) + '\n' + 'Confusion Matrix' + str(cm) + '\n'
+        print(log)
+        with open(log_path, 'a') as f:
+            f.write(log)
+
+
+run()
