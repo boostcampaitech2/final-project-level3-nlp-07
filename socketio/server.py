0699407fb5f00b52b230606902d7af0ab7082f79
Tentoto <p.isaac702@gmail.com>
2021-12-24 11:42:43 +0900

    Temp

--- socketio/server.py
+++ socketio/server.py
@@ -1,29 +1,41 @@
-from flask import Flask, render_template, templating,request,redirect
-import flask
+from flask import Flask, render_template, request,redirect
 import os
-from flask_socketio import SocketIO,join_room,leave_room,emit,send
-import logging
+from flask.wrappers import Response
+from flask_socketio import SocketIO,emit,join_room
 import ssl
+from pydub.utils import db_to_float
+from pydub.audio_segment import AudioSegment
 from password import password
-from multiprocessing import Process, Pipe
-from Stream_input import stream_input
-from threading import Thread
+from utils import inference
 from engineio.payload import Payload
+from struct import pack
+import io
+from flask_cors import CORS
 
 Payload.max_decode_packets = 100
-
-logger = logging.getLogger()
-
 
 static_files = {
     '/static': './public',
 }
+
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
-print(ROOT_PATH)
+
 STATIC_FOLDER = os.path.join(ROOT_PATH, "src/public")
 TEMPLATE_FOLDER = os.path.join(ROOT_PATH, "src/view")
-app=Flask(__name__,static_url_path='/public',static_folder=STATIC_FOLDER,template_folder=TEMPLATE_FOLDER)
 
+RATE = 16000
+CHUNK = 1024
+SILENCE_THRESH = db_to_float(-35)
+min_silence=int(RATE/CHUNK*0.2)
+endure=0
+frames=None
+ticker=False
+ind=0
+
+app=Flask(__name__,static_url_path='/public',static_folder=STATIC_FOLDER,template_folder=TEMPLATE_FOLDER)
+sio = SocketIO(app, async_mode='threading',cors_allowed_origins="*")
+CORS(app)
+infer=inference()
 @app.before_request
 def before_request():
     if request.url.startswith('http://'):
@@ -35,54 +47,106 @@ def before_request():
 def index():
   return render_template("index.html")
 
-sio = SocketIO(app, async_mode='threading',cors_allowed_origins="*")
-pipes={}
+@app.route('/',methods=['POST'])
+def streams():
+    global ind
+    global frames
+    global ticker
+    global endure
+    data=request.data
+    if len(data)%2==1:
+        data+=b'\x00'
+    with open('export_audios/test.wav','wb') as f:
+        f.write(data)
+        f.seek(0)
+    sound=AudioSegment.from_wav('export_audios/test.wav')
+    print(sound.rms)
+    try:
+        frames += sound
+    except:
+        frames=sound
+    if len(frames)>10000:
+            ticker=False
+            ind+=1
+            sio.start_background_task(emit('infer',infer(frames)))
+            print("##################inferred####################")
+            frames=None
+            endure=0
+    elif sound.rms<SILENCE_THRESH*32768:
+        if not ticker:
+            frames=frames[-64:]
+            endure=0
+        elif endure<min_silence:
+            endure+=1
+        elif len(frames)<=300:
+            pass
+        else:
+            ticker=False
+            ind+=1
+            sio.start_background_task(emit('infer',infer(frames)))
+            print("##################inferred####################")
+            frames=None
+            endure=0
+    else:
+        ticker=True
+        endure=0
+    return Response(status=200)
+
+
 @sio.on('connect')
 def connect():
-    print('connect')
+    print('connected')
 
 @sio.on('join_room')
-def join(data):
-    sid=request.sid
-    room = data['room']
-    join_room(room)
-    pipes[sid]={}
-    pipes[sid]["serv"],pipes[sid]["mdl"]=Pipe()
-    Process(target=stream_input,args=(sio,pipes[sid]["mdl"],sid,room)).start()
-    emit('joined',sid,to=sid)
-    emit("welcome",to=room,skip_sid=sid)
+def jr(sid,data):
+    join_room(data['room'])
+    sio.emit("welcome")
+    print('join_room')
 
-@sio.on('offer')
-def offer(offer,roomName):
-    sid=request.sid
-    emit('offer',offer,room=roomName,skip_sid=sid)
+@sio.on('start_stream')
+def start_stream():
+    print('start_stream')
 
-@sio.on('answer')
-def answer(answer,roomName):
-    sid=request.sid
-    emit('answer',answer,room=roomName,skip_sid=sid)
-
-@sio.on('ice')
-def ice(ice,roomName):
-    sid=request.sid
-    emit('ice',ice,room=roomName,skip_sid=sid)
-
-@sio.on('audio_stream')
-def stream(sid,stream,roomName):
-    print(len(stream),"LENLENLENLNENLENELENLENENL",len(stream))
-    pipes[sid]['serv'].send(stream)
-    if pipes[sid]['serv'].poll():
-        data=pipes[sid]['serv'].recv()
-        print('#########################################')
-        print(data)
-        print('#########################################')
-        emit('infer',data,room=roomName,skip_sid=sid)
-
-
-@sio.on('leave_room')
-def leave(data):
-    room = data['room']
-    leave_room(room)
+@sio.on('audio')
+def stream(sid,data,roomName):
+    global frames
+    global ticker
+    global endure
+    global ind
+    int_array=list(eval(data).values())
+    if int_array:
+        sound=int_array
+        srms=(sum([i**2 for i in sound])/len(sound))**0.5
+        print(srms,SILENCE_THRESH*32768)
+        try:
+            frames += sound
+        except:
+            frames=sound
+        if len(frames)>10000:
+                ticker=False
+                ind+=1
+                sio.start_background_task(emit('infer',infer(frames),to=roomName))
+                print("##################inferred####################")
+                frames=None
+                endure=0
+        elif srms<SILENCE_THRESH*32768:
+            if not ticker:
+                frames=frames[-64:]
+                endure=0
+            elif endure<min_silence:
+                endure+=1
+            elif len(frames)<=300:
+                pass
+            else:
+                ticker=False
+                ind+=1
+                sio.start_background_task(emit('infer',infer(frames),to=roomName))
+                print("##################inferred####################")
+                frames=None
+                endure=0
+        else:
+            ticker=True
+            endure=0
 
 if __name__ == '__main__':
     ssl_context=ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
--- socketio/src/public/js/app.js
+++ socketio/src/public/js/app.js
@@ -12,22 +12,11 @@ call.hidden=true;
 let myStream;
 let remoteStream;
 let muted = false;
-let roomName;
+const roomName=1;
 let myPeerConnection;
 let SID
 let mediaRecorder;
-
-let recLength = 0;
-let recBuffers = [];
-
-let bufferLen = 4096;
-let numChannels = 1;
-let mimeType = 'audio/wav';
-
-window.AudioContext = window.AudioContext || window.webkitAudioContext;
-let audio_context = new AudioContext();
-
-let audio_stream;
+let chunks=[];
 
 async function getMedia(){
     try{
@@ -38,6 +27,7 @@ async function getMedia(){
                 channelCount:1,
             }
         });
+        remoteAudio.srcObject = myStream;
     } catch(error){
         console.log(error);
     }
@@ -53,7 +43,7 @@ function handleHangupClick(){
     hangupBtn.style.display = "none";
     muteBtn.style.display = "none";
     mediaRecorder.stop();
-    socket.emit('leave_room',roomName);
+    socket.emit('leave_room',SID,roomName);
 }
 function handleMuteClick(){
     myStream
@@ -78,8 +68,6 @@ async function initCall(){
     welcome.hidden = true;
     call.hidden = false;
     await getMedia();
-    console.log('MEDIA DONE')
-    makeConnection();
     console.log("initCall done!");
 }
 
@@ -87,91 +75,43 @@ async function handleWelcomeSubmit(e){
     e.preventDefault();
     const input = welcomeForm.querySelector("input");
     await initCall();
-    socket.emit("join_room",{'room':input.value});
-    roomName = input.value;
+    socket.emit("join_room",SID,{'room':roomName});
     input.value = "";
 }
 
+
 welcomeForm.addEventListener("submit",handleWelcomeSubmit);
-socket.on('joined',(sid)=>{
-    console.log(sid)
-    SID=sid.toString();
+
+socket.on('connected',()=>{
+    console.log("connected");
+})
+
+socket.on('welcome',()=>{
     mediaRecorder=new MediaRecorder(myStream);
     mediaRecorder.ondataavailable=async (e)=>{
-        socket.emit('audio_stream',SID,e.data,roomName);
+        chunks.push(e.data);
+        var blob = new Blob(chunks, {
+            type: "audio/wav"
+        });
+        chunks=[];
+        var xmlhttp = new XMLHttpRequest();
+        var url = "https://115.85.182.137:6017/stream/";
+        xmlhttp.open("POST", url, true);
+        xmlhttp.setRequestHeader("Content-type", "audio/wav");
+        xmlhttp.setRequestHeader('Access-Control-Allow-Origin', '*')
+        xmlhttp.send(blob);
+        // var arr = await blob.arrayBuffer();
+        // if (arr.byteLength % 2 ===1) {
+        //     arr+='\x00';
+        // }
+        // var buffer = new Int16Array(arr);
+        //socket.emit('audio',SID,blob,roomName);
     }
-    mediaRecorder.start(63);
-})
-socket.on("welcome",async ()=>{
-    console.log("got welcome");
-    const offer = await myPeerConnection.createOffer();
-    myPeerConnection.setLocalDescription(offer);
-    console.log("sent the offer");
-    socket.emit("offer",offer,roomName);
-})
-
-socket.on("offer",async (offer)=>{
-    console.log("received the offer");
-    myPeerConnection.setRemoteDescription(offer);
-    const answer=await myPeerConnection.createAnswer();
-    myPeerConnection.setLocalDescription(answer);
-    socket.emit("answer",answer,roomName);
-    console.log("sent the answer");
-})
-
-socket.on("answer",(answer)=>{
-    console.log("received the answer");
-    myPeerConnection.setRemoteDescription(answer);
-})
-
-socket.on("ice",(ice)=>{
-    console.log("received candidate");
-    myPeerConnection.addIceCandidate(ice);
+    mediaRecorder.start(1000);
+    socket.emit('start_stream')
 })
 
 socket.on("infer",(data)=>{
     console.log('Received infer',data);
     displayer.innerText+=` ${data}`;
 })
-socket.on('err',(data)=>{
-    console.log('Received err',data);
-    displayer.innerText+=` ${data}`;
-})
-
-//RTC Code
-function makeConnection(){
-    myPeerConnection = new RTCPeerConnection({
-        iceServers:[
-            {
-                urls:[
-                    "stun:stun.l.google.com:19302",
-                    "stun:stun1.l.google.com:19302",
-                    "stun:stun2.l.google.com:19302",
-                    "stun:stun3.l.google.com:19302",
-                    "stun:stun4.l.google.com:19302",
-                ],
-            },
-        ]
-    });
-    myPeerConnection.addEventListener("icecandidate",handleICE);
-    myPeerConnection.addEventListener("addstream",handleStream);
-    myPeerConnection.addEventListener("addtrack",handleTrack);
-    myStream
-        .getAudioTracks()
-        .forEach((track) => {myPeerConnection.addTrack(track,myStream)});
-
-}
-
-function handleICE(event){
-    console.log("sent candidate");
-    socket.emit("ice",event.candidate,roomName);
-}
-
-function handleStream(event){
-    console.log("received track");
-    remoteAudio.srcObject = event.stream;
-}
-
-function handleTrack(event){
-    setTimeout(console.log("Track",event),10000);
-}
